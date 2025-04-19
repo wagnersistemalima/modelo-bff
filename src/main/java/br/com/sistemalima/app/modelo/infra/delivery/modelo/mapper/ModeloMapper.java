c6b6afcc2e882abb2fa69406ab42efe44d3d833f
wagnersistemalima <wagner.sistemalima@gmail.com>
2025-04-19 18:25:06 -0300

    ajustes interfaces mapper

--- src/main/java/br/com/sistemalima/app/modelo/infra/delivery/modelo/mapper/ModeloMapper.java
+++ src/main/java/br/com/sistemalima/app/modelo/infra/delivery/modelo/mapper/ModeloMapper.java
@@ -2,10 +2,11 @@ package br.com.sistemalima.app.modelo.infra.delivery.modelo.mapper;
 
 import br.com.sistemalima.app.modelo.core.domain.ModeloRequest;
 import br.com.sistemalima.app.modelo.core.domain.ModeloResponse;
+import br.com.sistemalima.app.modelo.core.errors.SummerParseException;
 import br.com.sistemalima.app.modelo.infra.delivery.modelo.dto.ModeloRequestDTO;
 import br.com.sistemalima.app.modelo.infra.delivery.modelo.dto.ModeloResponseDTO;
 
 public interface ModeloMapper {
-    ModeloRequest toRequest(ModeloRequestDTO modeloRequestDTO);
-    ModeloResponseDTO toResponseDTO(ModeloResponse modeloResponse);
+    ModeloRequest toRequest(ModeloRequestDTO modeloRequestDTO) throws SummerParseException;
+    ModeloResponseDTO toResponseDTO(ModeloResponse modeloResponse) throws SummerParseException;
 }
--- src/test/java/util/IntegrationTestUtil.java
+++ src/test/java/util/IntegrationTestUtil.java
@@ -7,6 +7,8 @@ import org.apache.commons.io.IOUtils;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+// utilitária usada para facilitar a leitura de arquivos JSON armazenados no classpath durante os testes de integração.
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class IntegrationTestUtil {
 
